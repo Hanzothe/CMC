0c780a939b19076e530e5f2f82b3213a560dad61
Hanzothe <gui_oliveirasantos123@hotmail.com>
2022-07-22 10:23:39 -0300

    Falta só a conta

--- src/components/Calcg.tsx
+++ src/components/Calcg.tsx
@@ -3,9 +3,15 @@ import "../global.css";
 import SelectBox from "./SelectBox";
 
 const Calcg = () => {
-  const [selectedInputMeasure, setSelectedInputMeasure] = useState([]); //o objeto selectedmeasure armazena o valor clicado na selectbox
-  const [a, setA] = useState<Number>(0);
-  const [optionsState, setOptionsState] = useState(0);
+  const [selectedInputMeasure, setSelectedInputMeasure] = useState(); //o objeto selectedmeasure armazena o valor clicado na selectbox
+  const [indexI, setIndexI] = useState<Number>(0); //indexI armazena o valor do Select dos ingredientes, carregando o index que vai identificar cada ingrediente, sendo [0] o primeiro elemento de optionsG, no caso, Farinha de Trigo
+  const [a, setA] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
+
+  let firstMeasure: number = 0;
+  let secondMeasure: number = 0;
+
+  const result: number = firstMeasure * secondMeasure;
 
   interface IOption {
     name: string;
@@ -98,31 +104,49 @@ const Calcg = () => {
       colherDeSobremesa: 5,
     },
   ];
-  const Sopa = optionsG.map((e) => e.colherSopa);
+  const Sopa = optionsG.map((e) => e.colherSopa); //Essas consts são um array com todos os valores de Colher de Chá
   const Cha = optionsG.map((e) => e.colherCha);
   const Xicara = optionsG.map((e) => e.xicara);
   const America = optionsG.map((e) => e.copoAmericano);
   const Sobre = optionsG.map((e) => e.colherDeSobremesa);
 
-  const X2 = Sopa[`${a}`];
-
   if (selectedInputMeasure == "colherSopa") {
-    console.log(X2);
+    firstMeasure = Sopa[`${indexI}`];
   }
   if (selectedInputMeasure == "colherCha") {
-    console.log(Cha[`${a}`]);
+    firstMeasure = Cha[`${indexI}`];
   }
   if (selectedInputMeasure == "xicara") {
-    console.log(Xicara[`${a}`]);
+    firstMeasure = Xicara[`${indexI}`];
   }
   if (selectedInputMeasure == "copoAmericano") {
-    console.log(America[`${a}`]);
+    firstMeasure = America[`${indexI}`];
   }
   if (selectedInputMeasure == "colherDeSobremesa") {
-    console.log(Sobre[`${a}`]);
+    firstMeasure = Sobre[`${indexI}`];
   }
 
-  console.log(selectedInputMeasure[0]); //Hanil, basicamente é tudo sobre isso: Eu queria poder acessar só o segundo valor ou só o primeiro do array pra poder terminar a lógica, tô na reta final
+  if (a == "colherSopa") {
+    secondMeasure = Sopa[`${indexI}`];
+  }
+  if (a == "colherCha") {
+    secondMeasure = Cha[`${indexI}`];
+  }
+  if (a == "xicara") {
+    secondMeasure = Xicara[`${indexI}`];
+  }
+  if (a == "copoAmericano") {
+    secondMeasure = America[`${indexI}`];
+  }
+  if (a == "colherDeSobremesa") {
+    secondMeasure = Sobre[`${indexI}`];
+  }
+
+  // console.log(firstMeasure);
+  // console.log(secondMeasure);
+  // // console.log(inputValue);
+  console.log(a);
+
   return (
     <>
       <div className="flex flex-col items-center">
@@ -130,7 +154,7 @@ const Calcg = () => {
         <span className="text-base ">O que você tem?</span>
         <select
           className="border-solid border-brandB text-sm border-2 h-10 w-60 rounded-lg text-black text-center"
-          onChange={(e) => setA(e.target.value)}
+          onChange={(e) => setIndexI(e.target.value)}
         >
           <option value="0">Farinha de Trigo</option>
           <option value="1">Açúcar</option>
@@ -145,12 +169,23 @@ const Calcg = () => {
         </select>
         <span className="text-base ">Quanto você tem?</span>
         <input
+          onChange={(e) => setInputValue(e.target.value)}
           type="Number"
           className="border-solid border-brandB text-sm border-2 h-10 w-60 rounded-lg text-black text-center"
         />
         <SelectBox onChangeCallback={setSelectedInputMeasure} />
         <span className="text-base ">Você quer converter em?</span>
-        <SelectBox />
+        <select
+          className="text-black  text-xl text-center rounded-lg h-10 w-60 relative   "
+          onChange={(e) => setA(e.target.value)}
+        >
+          <option value="">Selecione uma medida</option>
+          <option value="xicara"> Xícara de Chá </option>
+          <option value="copoAmericano">Copo Americano</option>
+          <option value="colherCha">Colher de Chá</option>
+          <option value="colherSopa">Colher de Sopa</option>
+          <option value="colherDeSobremesa">Colher de Sobremesa</option>
+        </select>
         <span className="text-base ">O Resultado da conversão!</span>
         <input
           type="Number"
